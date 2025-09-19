6649a289bf7bd0ac9afd007271018618a9987ee8
Queiyan <109931279+Queiyan@users.noreply.github.com>
2025-09-19 17:21:01 +0900

    코드 수정

--- 문제_코드처리하기/Solution.java
+++ 문제_코드처리하기/Solution.java
@@ -1,30 +1,25 @@
 class Solution {
     public String solution(String code) {
-             
         char[] codes = code.toCharArray();
-        String ret = "";
+        StringBuilder ret = new StringBuilder();
         int mode = 0;
-        
-        for(int idx = 0; idx < codes.length; idx++){          
-            
-            if(mode == 0){
-                if(codes[idx] != '1'){
-                    if(idx%2 == 0){
-                        ret = ret + codes[idx];
-                    }
-                } else{
+
+        for (int idx = 0; idx < codes.length; idx++) {
+            if (mode == 0) {
+                if (codes[idx] != '1') {
+                    if (idx % 2 == 0) ret.append(codes[idx]);
+                } else {
                     mode = 1;
                 }
-            } else{
-                if(codes[idx] != '1'){
-                    if(idx%2 == 1){
-                        ret = ret + codes[idx];
-                    }
-                } else{
+            } else { // mode == 1
+                if (codes[idx] != '1') {
+                    if (idx % 2 == 1) ret.append(codes[idx]);
+                } else {
                     mode = 0;
                 }
             }
-        } 
-        return ret;
+        }
+
+        return ret.length() == 0 ? "EMPTY" : ret.toString();
     }
 }
